1a60967ce070a28bcd6efb3d644ee8ed38f097a9
murplepuffin <kevinwin31@gmail.com>
2026-07-28 14:53:21 -0400

    add gitignore to reduce uneccessary dependencies

--- tests/random_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/kevinnguyen/Documents/quant study materials/projects/MonteCarloEngine/tests/random_tests[1]_tests.cmake")
-  include("/Users/kevinnguyen/Documents/quant study materials/projects/MonteCarloEngine/tests/random_tests[1]_tests.cmake")
-else()
-  add_test(random_tests_NOT_BUILT random_tests_NOT_BUILT)
-endif()
